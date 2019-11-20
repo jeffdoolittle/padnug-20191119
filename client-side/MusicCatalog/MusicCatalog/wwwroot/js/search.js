2975b5c647e03f4aa316c6560f8b709257b77bda
Jeff Doolittle <jeffdoolittle@gmail.com>
2019-11-19 18:45:50 -0800

    add albums html to search

--- client-side/MusicCatalog/MusicCatalog/wwwroot/js/search.js
+++ client-side/MusicCatalog/MusicCatalog/wwwroot/js/search.js
@@ -1,1 +1,27 @@
 ﻿
+fetch('api/Albums')
+  .then(response => response.json())
+  .then(data => {
+    const albumsHtml = data.map(album => `
+      <div class="column is-half">
+        <div class="box">
+          <div class="columns">
+            <div class="column">
+              <figure class="image is-square">
+                <img src="/img/${album.id}.png" alt="${album.title}" />
+              </figure>
+            </div>
+            <div class="column">
+              <h2 class="title">${album.title}</h2>
+              <h3 class="subtitle">${album.artist}</h3>
+              <div>
+                  Category: ${album.category}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
+
+    const html = albumsHtml.join(' ');
+  });
